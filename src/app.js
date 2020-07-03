1b000645701ff6f31b4c7565b323a3e2e833bc93
Ellen Styrélius <ellen.styrelius@gmail.com>
2020-07-03 11:11:14 +0200

    Import GlobalStyles in app.js and add some background styles and animation :)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,34 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components/macro'
+
+import { GlobalStyles } from './styles/global-styles'
+import { colors } from './styles/styles'
+
+const { lavender, salmon } = colors
+
+const ShiftingBackground = keyframes`
+  0% {
+   background: ${salmon}
+   };
+   50% {
+     background: ${lavender}
+   };
+   100% {
+   background: ${salmon}
+   };
+`
+
+const Page = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  animation: ${ShiftingBackground} 16s infinite linear;
+`
 
 export function App() {
-  return <div>Ellen Styrélius</div>
+  return (
+    <>
+      <GlobalStyles />
+      <Page>Ellen Styrélius</Page>)
+    </>
+  )
 }
