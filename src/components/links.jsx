c3e1ad1d62cf1ac9120f2f30639b659a80d7f38d
Ellen Styrélius <ellen.styrelius@gmail.com>
2020-07-04 23:33:53 +0200

    Add links.jsx with link to GitHub and LinkedIn. Import the component in page.jsx and add some flex styling stuffz to Container in order to make the Links component stick to bottom.

---
+++ src/components/links.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+import { fonts, colors } from '../styles/styles'
+
+const { black, darkGrey } = colors
+
+const Container = styled.section({
+  width: 480,
+  maxWidth: '100%',
+})
+
+const Link = styled.a({
+  padding: 8,
+  fontFamily: fonts.playfairDisplay,
+  color: black,
+  fontSize: 14,
+  textTransform: 'uppercase',
+  ':hover, :focus': {
+    color: darkGrey,
+    outline: 'none',
+  },
+})
+
+const links = [
+  { name: 'GitHub', url: 'https://github.com/styrelius' },
+  { name: 'LinkedIn', url: 'https://www.linkedin.com/in/ellen-styr%C3%A9lius-a55165183/' },
+]
+
+export function Links() {
+  return (
+    <Container>
+      {links.map(({ url, name }) => (
+        <Link href={url} target="_blank" key={url}>
+          {'> '}
+          {name}
+        </Link>
+      ))}
+    </Container>
+  )
+}
--- src/components/page.jsx
+++ src/components/page.jsx
@@ -5,19 +5,26 @@ import { colors } from '../styles/styles'
 
 import { Header } from './header'
 import { Presentation } from './presentation'
+import { Links } from './links'
 
 const Container = styled.div`
   background: ${colors.beige};
   width: 100%;
   min-height: calc(100vh - (${({ appPadding }) => appPadding}px * 2));
   padding: 32px 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `
 
 export function Page({ appPaddingTopBottom }) {
   return (
     <Container appPadding={appPaddingTopBottom}>
-      <Header />
-      <Presentation />
+      <div>
+        <Header />
+        <Presentation />
+      </div>
+      <Links />
     </Container>
   )
 }
