2fd030ab85a4ce38d5faef4ef86594c6defe1ea5
Ellen Styrélius <ellen.styrelius@gmail.com>
2020-07-08 12:53:39 +0200

    Change hue of darkGrey in colors object in styles.js.

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2,7 +2,7 @@ export const colors = {
   black: '#21201F',
   offWhite: '#FDFCFA',
   beige: '#FBF8ED',
-  darkGrey: '#54514E',
+  darkGrey: '#605D59',
   lavender: '#E9E7F2',
   salmon: '#FCEBDF',
 }
