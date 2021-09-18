20c28bd12fee8d818635c951693aa23720bbcddc
Ellen Styrélius <ellen.styrelius@gmail.com>
2021-09-18 21:25:11 +0200

    Replace styled-components template literal syntax with JS object syntax in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components/macro'
+import styled, { keyframes, css } from 'styled-components/macro'
 
 import { Page } from './components/page'
 import { GlobalStyles } from './styles/global-styles'
@@ -8,36 +8,40 @@ import { mobileScreen } from './styles/media-queries'
 
 const { lavender, salmon } = colors
 
-const ShiftingBackground = keyframes`
-  0% {
-   background: ${lavender}
-   };
-   50% {
-     background: ${salmon}
-   };
-   100% {
-   background: ${lavender}
-   };
-`
-const paddingTopBottom = 16
+const ShiftingBackground = keyframes({
+  '0%': {
+    background: lavender,
+  },
+  '45%': {
+    background: salmon,
+  },
+  '100%': {
+    background: lavender,
+  },
+})
 
-const Background = styled.div`
-  min-height: 100vh;
-  height: 100%;
-  width: 100%;
-  padding: ${paddingTopBottom}px 32px;
-  animation: ${ShiftingBackground} 120s infinite linear;
-  ${mobileScreen} {
-    padding: ${paddingTopBottom}px 12px;
-  }
-`
+const APP_PADDING_TOP_BOTTOM = 16
+
+const Background = styled.div({
+  minHeight: '100vh',
+  height: '100%',
+  width: '100%',
+  padding: `${APP_PADDING_TOP_BOTTOM}px 32px`,
+  animation: css`
+    ${ShiftingBackground} 120s infinite linear
+  `,
+
+  [mobileScreen]: {
+    padding: `${APP_PADDING_TOP_BOTTOM}px 12px`,
+  },
+})
 
 export function App() {
   return (
     <>
       <GlobalStyles />
       <Background>
-        <Page appPaddingTopBottom={paddingTopBottom} />
+        <Page appPaddingTopBottom={APP_PADDING_TOP_BOTTOM} />
       </Background>
     </>
   )
