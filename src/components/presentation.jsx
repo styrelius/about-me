b602ac61853ce76b15e8522c600b520a5b5f466f
Ellen Styrélius <ellen.styrelius@gmail.com>
2022-01-06 16:28:49 +0100

    Use another approach to the styling of paragraps in presentation.jsx (no visual changes)

--- src/components/presentation.jsx
+++ src/components/presentation.jsx
@@ -14,10 +14,12 @@ const Container = styled.section({
   justifyContent: 'center',
   alignItems: 'center',
 })
-
-const PresentationText = styled.div({
+const PresentationTextWrapper = styled.div({
   width: '100%',
   padding: '8px 0',
+})
+const PresentationText = styled.p({
+  display: 'inline',
   fontFamily: helveticaNeue,
   fontSize: fontSizes.md,
   fontWeight: 200,
@@ -27,15 +29,12 @@ const PresentationText = styled.div({
     fontSize: fontSizes.mobileMd,
   },
 })
-
 const FirstWords = styled.span({
   fontFamily: fonts.playfairDisplay,
 })
-
 const Spacer = styled.div({
   height: 24,
 })
-
 const Link = styled.a({
   ...linkStyles,
 })
@@ -43,33 +42,43 @@ const Link = styled.a({
 export function Presentation() {
   return (
     <Container>
-      <PresentationText>
-        <FirstWords>Hi, I'm Ellen</FirstWords> - web developer obsessed with most things UX.
-        Passionate about clean, readable, testable code, and elegant user-friendly interfaces. I
-        also have a Bachelor of Fine Arts.
-      </PresentationText>
-      <PresentationText>
-        Fan of typed systems and encapsulated components, who really enjoys figuring out how to
-        build scalable front-end architecture.
-      </PresentationText>
-      <PresentationText>
-        When I don't write code I like to spend my time drawing, sewing, going to exhibitions or
-        having dinner with friends.
-      </PresentationText>
+      <PresentationTextWrapper>
+        <FirstWords>Hi, I'm Ellen </FirstWords>
+        <PresentationText>
+          - web developer obsessed with most things UX. Passionate about clean, readable, testable
+          code, and elegant user-friendly interfaces. I also have a Bachelor of Fine Arts.
+        </PresentationText>
+      </PresentationTextWrapper>
+      <PresentationTextWrapper>
+        <PresentationText>
+          Fan of typed systems and encapsulated components, who really enjoys figuring out how to
+          build scalable front-end architecture.
+        </PresentationText>
+      </PresentationTextWrapper>
+      <PresentationTextWrapper>
+        <PresentationText>
+          When I don't write code I like to spend my time drawing, sewing, going to exhibitions or
+          having dinner with friends.
+        </PresentationText>
+      </PresentationTextWrapper>
       <Spacer />
-      <PresentationText>
-        <FirstWords>Currently: </FirstWords>
-        <Link target="_blank" href="https://www.hedvig.com/">
-          Hedvig
-        </Link>
-        , Stockholm
-      </PresentationText>
-      <PresentationText>
-        <FirstWords>Side project: </FirstWords>
-        <Link target="_blank" href="https://clay.so/">
-          Clay
-        </Link>
-      </PresentationText>
+      <PresentationTextWrapper>
+        <PresentationText>
+          <FirstWords>Currently: </FirstWords>
+          <Link target="_blank" href="https://www.hedvig.com/">
+            Hedvig
+          </Link>
+          , Stockholm
+        </PresentationText>
+      </PresentationTextWrapper>
+      <PresentationTextWrapper>
+        <PresentationText>
+          <FirstWords>Side project: </FirstWords>
+          <Link target="_blank" href="https://clay.so/">
+            Clay
+          </Link>
+        </PresentationText>
+      </PresentationTextWrapper>
     </Container>
   )
 }
