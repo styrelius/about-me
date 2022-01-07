61cb79598de24e66e071fec45aa09c3dd7178d1f
Ellen Styrélius <ellen.styrelius@gmail.com>
2022-01-07 13:46:38 +0100

    Fix layout in page.tsx

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -27,7 +27,7 @@ const ContentWrapper = styled.div({
   width: PAGE_CONTENT_WRAPPER_WIDTH,
   maxWidth: '100%',
   display: 'grid',
-  gridTemplateRows: '1fr auto 1.5fr',
+  gridTemplateRows: 'auto auto 120px',
   gap: spacingBaseValue * 5,
 })
 
